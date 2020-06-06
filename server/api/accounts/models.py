df80b76407ed4895884946325477738205829e7a
guilherme <guilherme@guilhermeweb.dev>
2020-06-06 15:16:08 -0300

    Implementação das requisições de amizade

--- server/api/accounts/models.py
+++ server/api/accounts/models.py
@@ -1,29 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, Group
+from django.utils.translation import gettext as _
+
 
 class GroupModel(models.Model):
-    name: models.CharField(blank=True, max_length=128)
-    admin: models.ManyToManyField(User, on_delete=models.SET_NULL, null=True)
-    registration_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(blank=True, max_length=128)
+    admin = models.ManyToManyField('UserModel')
+    registration_date = models.DateTimeField(auto_now=True)
 
-class UserModel(models.Model):
-    username = models.CharField(blank=True, max_length=128)
-    first_name = models.CharField(blank=True, max_length=128)
-    last_name = models.CharField(blank=True, max_length=128)
+
+class UserModel(AbstractUser):
     avatar = models.ImageField(upload_to='avatars', blank=True)
-    birth = models.DateField(blank=True, default='1990-01-01')
+    birth = models.DateField(blank=True, null=True)
     city = models.CharField(blank=True, max_length=128)
-    choice = models.CharField(blank=True, max_length=128,
+    meritial_state = models.CharField(blank=True, max_length=128,
                                     choices=[('1', 'married'),
                                              ('2', 'in relationship'),
                                              ('3', 'single')])
-    meritial_state = models.ManyToManyField('self', blank=True, related_name='choice')
-    friends = models.ManyToManyField(User, on_delete=models.SET_NULL, null=True)
-    groups = models.ManyToManyField(GroupModel, on_delete=models.SET_NULL, null=True)
+    groups = models.ManyToManyField(GroupModel)
+
+    class Meta(AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
+        abstract = False
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+
+
+class FriendRequest(models.Model):
+    user = models.ForeignKey(
+        UserModel, 
+        on_delete=models.CASCADE,
+        related_name="friends"
+    )
+    accepted = models.BooleanField(default=False)
+    registration_date = models.DateTimeField(auto_now=True)
+
 
 class PostModel(models.Model):
-    username = models.ManyToManyField(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name="posts")
     text = models.CharField(blank=True, max_length=256)
     image = models.ImageField(upload_to='image', blank=True)
     registration_date = models.DateTimeField(auto_now_add=True)
-
